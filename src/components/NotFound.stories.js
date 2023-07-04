ae1aeecf726aa5f04c841857a9bebcac2efed6c7
Sun-007 <138206213+Umesh-WW@users.noreply.github.com>
2023-07-04 11:25:56 +0530

    remove warning for not found

--- src/components/NotFound.stories.js
+++ src/components/NotFound.stories.js
@@ -3,11 +3,6 @@ import { action } from '@storybook/addon-actions';
 import { Button } from 'react-bootstrap';
 import NotFound from './NotFound';
 
-export default {
-	component: NotFound,
-	title: 'NotFound',
-};
-
 export const NotFoundWithoutProps = () => (
 	<NotFound onClick={action('clicked')}>
 		<Button>Back to home</Button>
@@ -15,18 +10,29 @@ export const NotFoundWithoutProps = () => (
 );
 
 export const NotFoundWithTitle = () => (
-	<NotFound onClick={action('clicked')} jumbotronProps={{title: 'Not found!!'}}>
+	<NotFound
+		onClick={action('clicked')}
+		jumbotronProps={{ title: 'Not found!!' }}
+	>
 		<Button>Custom title</Button>
 	</NotFound>
 );
 
 export const NotFoundWithDescription = () => (
-	<NotFound onClick={action('clicked')} jumbotronProps={{
-		title: 'Not found!!',
-		description: "A custom description is a description written by a player to describe their own character, an indoors location or an item. Custom descriptions for characters were the first of these to get implemented"
-	}}>
+	<NotFound
+		onClick={action('clicked')}
+		jumbotronProps={{
+			title: 'Not found!!',
+			description:
+				'A custom description is a description written by a player to describe their own character, an indoors location or an item. Custom descriptions for characters were the first of these to get implemented'
+		}}
+	>
 		<Button>Custom description</Button>
 	</NotFound>
 );
+const NotFoundComponent = {
+	component: NotFound,
+	title: 'NotFound'
+};
 
-
+export default NotFoundComponent;
